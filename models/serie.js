ec6db697ede35b1525761a0664daaaf67763f588
rodolpop <garciasena@yahoo.com>
2022-03-29 12:01:49 +0200

    serie1

--- models/serie.js
+++ models/serie.js
@@ -1,10 +1,15 @@
-//serie model
+
 
 class Serie {
     constructor(name) {
 
-//...
+        this.name = name;
+        this.id = new Date().toISOString(); 
+        this.startTime = null; 
+        this.endTime = null; 
 
     }
 }
 module.exports = Serie;
+
+    
