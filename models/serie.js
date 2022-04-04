e558d99962adf2b3ad3da2fffbf5af83c73e5bcd
rodolpop <garciasena@yahoo.com>
2022-04-04 06:21:39 +0200

    Serie class C

--- models/serie.js
+++ models/serie.js
@@ -3,8 +3,14 @@
 class Serie {
     constructor(name) {
 
-//...
+        this.name = name;
+        this.id = new Date().toISOString(); 
+        this.startTime = null; 
+        this.endTime = null; 
+
+
 
     }
 }
+
 module.exports = Serie;
