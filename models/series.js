f055d5c96d7b9b921bfb30cab432514c14b0d0b6
DANI MI <zeleugim@gmail.com>
2022-04-06 13:41:44 +0200

    a

--- models/series.js
+++ models/series.js
@@ -1,3 +1,6 @@
+//serie services
+//CRUD 
+
 const Serie = require('./serie');
 
 class Series {
@@ -17,6 +20,21 @@ class Series {
         });
     }
 
+    addSerie( name ) {
+
+        const serie = new Serie(name);
+        this._listaSeries.push(serie)
+    }
+
+    deleteSerie( id = '' ) {
+        for (let i = 0; i < this._listaSeries.length; i++) {
+            if (this._listaSeries[i].id === id) {
+                delete this._listaSeries[i] //me genera un null en el JSON
+                this._listaSeries = this._listaSeries.filter(e => e!== null)
+            }
+        }
+    }
+
     serieList() { 
         this._listaSeries.forEach( (tarea, i) => {
             const num = `Serie nº${i + 1}:`.green;
@@ -29,6 +47,7 @@ class Series {
             console.log(`${ num } ${ name } => ${ state }`);
         });        
     }
+
     serieListByType(type) { 
         this._listaSeries.forEach( (tarea, i) => {
             const num = `Serie nº${i + 1}:`.green;
@@ -56,7 +75,6 @@ class Series {
         }
     }
 
-
     endSerie( ids = [] ) { //recibe un array con 
         for (let i = 0; i < ids.length; i++) {
                 for (let j = 0; j < this._listaSeries.length; j++) {
@@ -71,4 +89,7 @@ class Series {
         }
     }
 }
+
+
+
 module.exports = Series;
